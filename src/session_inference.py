477c8bc5fc4b3905296b22278ab519965909651c
almaz <almazgimaev@gmail.com>
2025-02-21 13:16:45 +0100

    Update Docker image version, add requirements file, and modify model data for custom inference

--- src/session_inference.py
+++ src/session_inference.py
@@ -11,7 +11,9 @@ api = sly.Api()
 image_path = "src/demo_data/input/2.jpg"  # ⬅︎ Put your image path here
 app_url = "http://localhost:8000"
 
-session = sly.nn.inference.SessionJSON(api, session_url=app_url, inference_settings={"return_heatmaps": True})
+session = sly.nn.inference.SessionJSON(
+    api, session_url=app_url, inference_settings={"return_heatmaps": True}
+)
 
 prediction = session.inference_image_path(image_path)
 print(prediction)
